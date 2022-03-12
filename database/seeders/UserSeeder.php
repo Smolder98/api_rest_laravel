941c987921de33a4440bfe6ef3093699e90de54e
Smolder98 <alicoescobar@gmail.com>
2022-03-12 10:02:22 -0600

    agregando usuarios al seed

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -16,20 +16,33 @@ class UserSeeder extends Seeder
 
         DB::table("users")->insert([
             [
-            "idFirebase"  => "4645465645656",
+            "idFirebase"  => "dhjdhdwidhh3343",
             "name" =>"Abdiel",
             "lastname" =>"Licona",
             "numberAccount" =>"201910020280",
             "phone" =>"99693345",
-            "status" =>"fdfdfdfd",
-            "image" =>"fdfdfdf",
-            "address" =>"dfdfdf",
-            "birthDate" =>"dfdfdfdfdfdf",
+            "status" =>"activo",
+            "image" =>"image",
+            "address" =>"direccion",
+            "birthDate" =>"13/06/2000",
             "carrera" =>"Computacion",
             "email" =>"correo@correo.com",
             "password" =>"123"
+            ],
+             [
+            "idFirebase"  => "dhjdhdwi454hh3343",
+            "name" =>"Angel",
+            "lastname" =>"Garcia",
+            "numberAccount" =>"201910020250",
+            "phone" =>"97893345",
+            "status" =>"activo",
+            "image" =>"image",
+            "address" =>"direccion",
+            "birthDate" =>"13/06/2000",
+            "carrera" =>"Computacion",
+            "email" =>"correo2@correo.com",
+            "password" =>"123"
             ]
-
         ]);
     }
 }
